53befdbf08be3d5c0017ecae3c8acfefa1ef3752
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-14 09:59:09 +0100

    desc

--- basics.sql
+++ basics.sql
@@ -21,8 +21,9 @@ WHERE students.school = (
     SELECT school FROM students WHERE nom = 'Addy'
 );
 -- Affiche uniquement les prénoms des étudiants, par ordre inverse à l’alphabet (DESC). Ensuite, la même chose mais en limitant les résultats à 2.
-
+select prenom from students order by prenom desc;
 -- Ajoute Ginette Dalor, née le 01/01/1930 et affecte-la à Bruxelles, toujours en SQL.
+
 -- Modifie Ginette (toujours en SQL) et change son sexe et son prénom en “Omer”.
 -- Supprimer la personne dont l’ID est 3.
 -- Modifier le contenu de la colonne School de sorte que "1" soit remplacé par "Liege" et "2" soit remplacé par "Gent". (attention au type de la colonne !)

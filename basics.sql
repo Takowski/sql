ab19e08ad3f1d5601cfedc0298f14ee23bbcdf86
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-14 10:14:34 +0100

    liege/gent

--- basics.sql
+++ basics.sql
@@ -28,7 +28,10 @@ VALUES ('Dalor', 'Ginette', '1930-01-01', 'F', 1);
 -- Modifie Ginette (toujours en SQL) et change son sexe et son prénom en “Omer”.
 UPDATE students SET prenom = 'Omer', genre = 'M' WHERE nom = 'Dalor';
 -- Supprimer la personne dont l’ID est 3.
+DELETE FROM students WHERE idStudent = 3;
 -- Modifier le contenu de la colonne School de sorte que "1" soit remplacé par "Liege" et "2" soit remplacé par "Gent". (attention au type de la colonne !)
+UPDATE school SET school = 'Liege' WHERE idschool = 1;
+UPDATE school SET school = 'Gent' WHERE idschool = 2;
 -- Faire d’autres manipulations pour voir si t’es bien compris.
 
 

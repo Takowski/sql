e6a41605f697b4adeb94400d26414c251f4babbb
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-14 10:07:30 +0100

    omer

--- basics.sql
+++ basics.sql
@@ -23,8 +23,10 @@ WHERE students.school = (
 -- Affiche uniquement les prénoms des étudiants, par ordre inverse à l’alphabet (DESC). Ensuite, la même chose mais en limitant les résultats à 2.
 select prenom from students order by prenom desc;
 -- Ajoute Ginette Dalor, née le 01/01/1930 et affecte-la à Bruxelles, toujours en SQL.
-
+INSERT INTO students (nom, prenom, datenaissance, genre, school)
+VALUES ('Dalor', 'Ginette', '1930-01-01', 'F', 1);
 -- Modifie Ginette (toujours en SQL) et change son sexe et son prénom en “Omer”.
+UPDATE students SET prenom = 'Omer', genre = 'M' WHERE nom = 'Dalor';
 -- Supprimer la personne dont l’ID est 3.
 -- Modifier le contenu de la colonne School de sorte que "1" soit remplacé par "Liege" et "2" soit remplacé par "Gent". (attention au type de la colonne !)
 -- Faire d’autres manipulations pour voir si t’es bien compris.
